b8e6c0d510f05deb438ff4feca588ebf07f44249
tdoval <thiagodovalaraujo@gmail.com>
2024-02-01 20:25:12 -0300

    @feature/API request to backend with Axios

--- react-frontend/src/SearchForm.jsx
+++ react-frontend/src/SearchForm.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useForm } from "react-hook-form"
+import axios from 'axios';
 
 function SearchForm() {
     const { register, errors, handleSubmit } = useForm();
-    const onSubmit = (data) => {
-        console.log("RESULT>", data);
-        alert(JSON.stringify(data));
+
+    const onSubmit = async (data) => {
+
+        try {
+            const response = await axios.post('http://localhost:3000/google/search', data);
+            console.log('API Response:', response.data);
+            alert(JSON.stringify(response.data));
+        } catch (error) {
+            console.error('API Error:', error);
+        }
     };
     console.log(errors);
 
@@ -14,17 +22,17 @@ function SearchForm() {
             <label>Localidade</label>
             <input
                 type="text"
-                {...register("Localidade", { required: true, maxLength: 100 })}
+                {...register("localidade", { required: true, maxLength: 100 })}
             />
             <label>Frequência</label>
             <input
                 type="text"
-                {...register("Frequência", { required: true, maxLength: 100 })}
+                {...register("frequencia", { required: true, maxLength: 100 })}
             />
             <label>Palavra Chave</label>
             <input
                 type="text"
-                {...register("Palavra Chave", { required: true, maxLength: 100 })}
+                {...register("palavraChave", { required: true, maxLength: 100 })}
             />
             <input value="Buscar" type="submit" />
         </form>
