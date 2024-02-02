a055dddba22b06bed81178333cdec2fcbc9fa791
tdoval <thiagodovalaraujo@gmail.com>
2024-02-02 12:59:37 -0300

    @feature/ajustes para retorno da busca

--- google-robot/main.go
+++ google-robot/main.go
@@ -10,11 +10,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func simulateGoogleSearch(location string, frequency int, keywords []string) {
+type RespostaGoogle struct {
+	Localidade    string `json:"localidade"`
+	Frequencia    int    `json:"frequencia"`
+	PalavrasChave string `json:"palavrasChave"`
+	CodigoStatus  int    `json:"codigoStatus"`
+	CorpoResposta string `json:"corpoResposta"`
+}
+
+func simulateGoogleSearch(location string, frequency int, keywords string) RespostaGoogle {
 	fmt.Printf("Simulando pesquisa no Google com os seguintes parâmetros:\n")
 	fmt.Printf("Localidade: %s\n", location)
 	fmt.Printf("Frequência: %d\n", frequency)
-	fmt.Printf("Palavras-chave: %v\n", keywords)
+	fmt.Printf("Palavras-chave: %s\n", keywords)
 
 	apiURL := "https://cse.google.com/cse?cx=a4823638e4af341f2"
 	apiKey := os.Getenv("GOOGLE_API_KEY")
@@ -26,7 +34,7 @@ func simulateGoogleSearch(location string, frequency int, keywords []string) {
 	resp, err := http.Get(queryURL)
 	if err != nil {
 		fmt.Println("Erro na chamada simulada à API do Google:", err)
-		return
+		return RespostaGoogle{}
 	}
 	defer resp.Body.Close()
 
@@ -38,7 +46,16 @@ func simulateGoogleSearch(location string, frequency int, keywords []string) {
 	} else {
 		fmt.Printf("Corpo da Resposta: %s\n", body)
 	}
-	// Aqui, você pode processar o corpo da resposta se necessário
+
+	resposta := RespostaGoogle{
+		Localidade:    location,
+		Frequencia:    frequency,
+		PalavrasChave: keywords,
+		CodigoStatus:  resp.StatusCode,
+		CorpoResposta: string(body),
+	}
+
+	return resposta
 
 }
 
@@ -46,12 +63,10 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/search", func(c *gin.Context) {
-		// Obtenha os parâmetros da consulta da solicitação
 		location := c.Query("location")
 		frequencyStr := c.Query("frequency")
-		keywords := c.QueryArray("keywords")
+		keywords := c.Query("keywords")
 
-		// Verifique se os parâmetros obrigatórios estão presentes
 		if location == "" || frequencyStr == "" {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": "Localidade e Frequência são obrigatórios",
@@ -59,7 +74,6 @@ func main() {
 			return
 		}
 
-		// Converta a string frequency para um tipo int
 		frequency, err := strconv.Atoi(frequencyStr)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -68,18 +82,12 @@ func main() {
 			return
 		}
 
-		// Lógica para simular pesquisa no Google
-		simulateGoogleSearch(location, frequency, keywords)
+		resposta := simulateGoogleSearch(location, frequency, keywords)
 
-		// Exemplo: Imprimir a mensagem de pesquisa bem-sucedida
 		fmt.Println("Simulação de pesquisa bem-sucedida")
 
-		// Responda com sucesso
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Pesquisa bem-sucedida",
-		})
+		c.JSON(http.StatusOK, resposta)
 	})
 
-	// Inicie o servidor na porta 8080
 	router.Run(":8080")
 }
